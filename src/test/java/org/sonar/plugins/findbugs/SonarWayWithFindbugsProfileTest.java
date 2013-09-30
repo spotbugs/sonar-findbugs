10df4d0275fb62538d8309f73404c3be946582e1
Dinesh Bolkensteyn <dinesh@dinsoft.net>
2013-09-30 17:52:03 +0200

    SONARJAVA-303 Rule: "equals(Object obj)" should be overridden as soon as method "compareTo(T obj)" is defined

--- src/test/java/org/sonar/plugins/findbugs/SonarWayWithFindbugsProfileTest.java
+++ src/test/java/org/sonar/plugins/findbugs/SonarWayWithFindbugsProfileTest.java
@@ -34,7 +34,7 @@ public class SonarWayWithFindbugsProfileTest {
     ValidationMessages validation = ValidationMessages.create();
     RulesProfile profile = sonarWayWithFindbugs.createProfile(validation);
     assertThat(profile.getActiveRulesByRepository(FindbugsConstants.REPOSITORY_KEY))
-        .hasSize(390);
+      .hasSize(389);
     assertThat(validation.hasErrors()).isFalse();
   }
 
