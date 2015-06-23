d0ddb9a166aed5974fe1f5291bd46f9156675f2f
David Gageot <david@gageot.net>
2015-06-23 18:36:21 +0200

    Run ITs on TravisCI

---
+++ its/plugin/src/test/java/com/sonar/it/java/suite/FBContribTest.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (C) 2014-2014 SonarSource SA
+ * All rights reserved
+ * mailto:contact AT sonarsource DOT com
+ */
+package com.sonar.it.java.suite;
+
+import com.sonar.orchestrator.Orchestrator;
+import com.sonar.orchestrator.build.MavenBuild;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.sonar.wsclient.Sonar;
+import org.sonar.wsclient.issue.Issue;
+import org.sonar.wsclient.issue.IssueClient;
+import org.sonar.wsclient.issue.IssueQuery;
+import org.sonar.wsclient.services.Measure;
+import org.sonar.wsclient.services.ResourceQuery;
+
+import java.util.List;
+
+import static org.fest.assertions.Assertions.assertThat;
+import static org.junit.Assume.assumeTrue;
+
+public class FBContribTest {
+
+  @ClassRule
+  public static Orchestrator orchestrator = FindbugsTestSuite.ORCHESTRATOR;
+
+  private static final String PROJECT_KEY = "org.sonar.tests:fb-contrib";
+
+  @Before
+  public void deleteData() {
+    orchestrator.resetData();
+  }
+
+  private static String keyFor(String s) {
+    if (orchestrator.getConfiguration().getSonarVersion().isGreaterThanOrEquals("4.2")) {
+      return PROJECT_KEY + ":src/" + s + ".java";
+    } else if (orchestrator.getConfiguration().getPluginVersion("java").isGreaterThanOrEquals("2.1")) {
+      return PROJECT_KEY + ":" + s + ".java";
+    } else {
+      return PROJECT_KEY + ":[default]." + s;
+    }
+  }
+
+  @Test
+  public void test() throws Exception {
+    assumeTrue(orchestrator.getConfiguration().getPluginVersion("findbugs").isGreaterThan("2.3"));
+    MavenBuild build = MavenBuild.create()
+      .setPom(TestUtils.projectPom("simple"))
+      .setProperty("sonar.profile", "IT")
+      .setProperty("sonar.dynamicAnalysis", "false")
+      .setProperty("sonar.findbugs.confidenceLevel", "low")
+      .setCleanPackageSonarGoals();
+    orchestrator.executeBuild(build);
+
+    boolean findbugsGreaterThan3_2 = orchestrator.getConfiguration().getPluginVersion("findbugs").isGreaterThanOrEquals("3.2");
+    int numberViolations = findbugsGreaterThan3_2 ? 3 : 2;
+
+    Sonar wsClient = orchestrator.getServer().getWsClient();
+    assertThat(wsClient.find(new ResourceQuery(PROJECT_KEY)).getVersion()).isEqualTo("0.1-SNAPSHOT");
+    assertThat(getProjectMeasure("violations").getIntValue()).isEqualTo(numberViolations);
+    IssueClient issueClient = orchestrator.getServer().wsClient().issueClient();
+    List<Issue> issues = issueClient.find(IssueQuery.create()).list();
+    assertThat(issues).hasSize(numberViolations);
+    issues = issueClient.find(IssueQuery.create().components(keyFor("Simple")).rules("fb-contrib:IPU_IMPROPER_PROPERTIES_USE_SETPROPERTY")).list();
+    // SONARJAVA-216:
+    assertThat(issues).hasSize(1);
+    assertThat(issues.get(0).message()).isEqualTo("Method Simple.method() uses Properties.put instead of Properties.setProperty");
+
+    if (findbugsGreaterThan3_2) {
+      issues = issueClient.find(IssueQuery.create().components(keyFor("Simple")).rules("findsecbugs:PREDICTABLE_RANDOM")).list();
+      assertThat(issues).hasSize(1);
+      assertThat(issues.get(0).message()).isEqualTo("Use of java.util.Random is predictable.");
+    }
+  }
+
+  protected Measure getProjectMeasure(String metricKey) {
+    return orchestrator.getServer().getWsClient().find(ResourceQuery.createForMetrics(PROJECT_KEY, metricKey)).getMeasure(metricKey);
+  }
+
+}
