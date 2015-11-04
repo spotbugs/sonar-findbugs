c9ca8ec320059336c602cb932a17284735e60150
Massimo Paladin <massimo.paladin@gmail.com>
2015-11-04 15:20:49 +0100

    Run pr-analysis on travis job

--- travis.sh
+++ travis.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 function installTravisTools {
   mkdir ~/.local
-  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v16 | tar zx --strip-components 1 -C ~/.local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v19 | tar zx --strip-components 1 -C ~/.local
   source ~/.local/bin/install
 }
 
@@ -12,6 +12,21 @@ case "$TEST" in
 
 ci)
   mvn verify -B -e -V
+  # pr analysis
+  if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+    source $HOME/.jdk_switcher_rc
+    jdk_switcher use oraclejdk8
+    mvn -V -B -e verify sonar:sonar \
+      -DskipTests -Djacoco.skip -DskipObfuscation -Denforcer.skip \
+      -Dsonar.analysis.mode=preview \
+      -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
+      -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
+      -Dsonar.github.login=$SONAR_GITHUB_LOGIN \
+      -Dsonar.github.oauth=$SONAR_GITHUB_OAUTH \
+      -Dsonar.host.url=$SONAR_HOST_URL \
+      -Dsonar.login=$SONAR_LOGIN \
+      -Dsonar.password=$SONAR_PASSWD
+  fi
   ;;
 
 plugin|ruling)
