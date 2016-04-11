0cce1f6399e648cdd066d7899b3076eb31bde2dc
Nicolas PERU <nicolas.peru@sonarsource.com>
2016-04-11 10:15:26 +0200

    Simplify CI

--- travis.sh
+++ travis.sh
@@ -12,36 +12,7 @@ case "$TEST" in
 
 ci)
   mvn verify -B -e -V
-  # pr analysis
-  if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-    source $HOME/.jdk_switcher_rc
-    jdk_switcher use oraclejdk8
-    mvn -V -B -e verify sonar:sonar \
-      -DskipTests -Djacoco.skip -DskipObfuscation -Denforcer.skip \
-      -Dsonar.analysis.mode=preview \
-      -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
-      -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
-      -Dsonar.github.login=$SONAR_GITHUB_LOGIN \
-      -Dsonar.github.oauth=$SONAR_GITHUB_OAUTH \
-      -Dsonar.host.url=$SONAR_HOST_URL \
-      -Dsonar.login=$SONAR_LOGIN \
-      -Dsonar.password=$SONAR_PASSWD
-  fi
   ;;
-
-plugin|ruling)
-  installTravisTools
-
-  mvn package -Dsource.skip=true -Denforcer.skip=true -Danimal.sniffer.skip=true -Dmaven.test.skip=true
-
-  if [ "$SQ_VERSION" = "DEV" ] ; then
-    build_snapshot "SonarSource/sonarqube"
-  fi
-
-  cd its/$TEST
-  mvn package -DjavaVersion="LATEST_RELEASE" -Dsonar.runtimeVersion="$SQ_VERSION" -Dmaven.test.redirectTestOutputToFile=false
-  ;;
-
 *)
   echo "Unexpected TEST mode: $TEST"
   exit 1
