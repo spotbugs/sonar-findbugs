904502a6e74e10281f511ef6712c5ec08c8f8295
Philippe Arteau <parteau@gosecure.ca>
2016-06-26 07:11:59 -0400

    Update FB-Contrin to version 6.2.3 (Rules set using the automate script)

--- src/test/java/org/sonar/plugins/findbugs/FbContribRulesDefinitionTest.java
+++ src/test/java/org/sonar/plugins/findbugs/FbContribRulesDefinitionTest.java
@@ -41,7 +41,7 @@ public class FbContribRulesDefinitionTest {
     assertThat(repository.language()).isEqualTo(Java.KEY);
 
     List<Rule> rules = repository.rules();
-    assertThat(rules).hasSize(215);
+    assertThat(rules).hasSize(231);
 
     for (Rule rule : rules) {
       assertThat(rule.key()).isNotNull();
