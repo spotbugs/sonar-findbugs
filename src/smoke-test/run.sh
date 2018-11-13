80edfcedfea4eabb59e5d5b6578bf36788530a36
Kengo TODA <skypencil@gmail.com>
2018-11-14 07:47:22 +0800

    reproduce reported issue by smoke test

---
+++ src/smoke-test/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Copied from https://github.com/SonarSource/docker-sonarqube/tree/master/7.1-alpine under LGPL
+
+set -e
+
+if [ "${1:0:1}" != '-' ]; then
+  exec "$@"
+fi
+
+chown -R sonarqube:sonarqube $SONARQUBE_HOME
+exec gosu sonarqube \
+  java -jar lib/sonar-application-$SONAR_VERSION.jar \
+  -Dsonar.log.console=true \
+  -Dsonar.jdbc.username="$SONARQUBE_JDBC_USERNAME" \
+  -Dsonar.jdbc.password="$SONARQUBE_JDBC_PASSWORD" \
+  -Dsonar.jdbc.url="$SONARQUBE_JDBC_URL" \
+  -Dsonar.web.javaAdditionalOpts="$SONARQUBE_WEB_JVM_OPTS -Djava.security.egd=file:/dev/./urandom" \
+  "$@"
