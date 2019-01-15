b09fe035aa4499498223ede2b8f5324901784349
Nicolas PERU <nicolas.peru@sonarsource.com>
2019-01-15 16:18:10 +0100

    SONARHTML-94 remove jsp langage declaration
    Langage is declared in sonar-html which is shipped by default in sonarqube 7.6

--- src/main/java/org/sonar/plugins/findbugs/language/Jsp.java
+++ src/main/java/org/sonar/plugins/findbugs/language/Jsp.java
@@ -19,77 +19,16 @@
  */
 package org.sonar.plugins.findbugs.language;
 
-import com.google.common.collect.Lists;
-import org.apache.commons.lang.StringUtils;
-import org.sonar.api.config.Configuration;
-import org.sonar.api.resources.AbstractLanguage;
-
-import java.util.List;
-
 /**
  * This language cover JavaServer Pages (JSP).
  *
- * It may conflicts with the "web" language from the Web Plugin which supports multiples XML derived languages.
+ * As sonar-html, which defines JSP lang is installed by default on sonarqube 7.6, JSP language will be always available.
  */
-public class Jsp extends AbstractLanguage {
+public class Jsp {
 
     /**
      * JSP key
      */
     public static final String KEY = "jsp";
-
-    /**
-     * JSP language name (Acronym chosen over "JavaServer Pages" to avoid visual glitch)
-     */
-    public static final String NAME = "JSP";
-
-
-    /**
-     * Key of the file suffix parameter
-     */
-    public static final String FILE_SUFFIXES_KEY = "sonar.jsp.file.suffixes";
-
-    /**
-     * Default Java files knows suffixes
-     */
-    public static final String DEFAULT_FILE_SUFFIXES = ".jsp";
-
-    /**
-     * Configuration of the plugin.
-     */
-    private final Configuration config;
-
-    /**
-     * Default constructor
-     */
-    public Jsp(Configuration config) {
-        super(KEY, NAME);
-        this.config = config;
-    }
-
-    /**
-     * {@inheritDoc}
-     *
-     * @see org.sonar.api.resources.AbstractLanguage#getFileSuffixes()
-     */
-    @Override
-    public String[] getFileSuffixes() {
-        String[] suffixes = filterEmptyStrings(config.getStringArray(FILE_SUFFIXES_KEY));
-        if (suffixes.length == 0) {
-            suffixes = StringUtils.split(DEFAULT_FILE_SUFFIXES, ",");
-        }
-        return suffixes;
-    }
-
-    private static String[] filterEmptyStrings(String[] stringArray) {
-        List<String> nonEmptyStrings = Lists.newArrayList();
-        for (String string : stringArray) {
-            if (StringUtils.isNotBlank(string.trim())) {
-                nonEmptyStrings.add(string.trim());
-            }
-        }
-        return nonEmptyStrings.toArray(new String[nonEmptyStrings.size()]);
-    }
-
 
 }
