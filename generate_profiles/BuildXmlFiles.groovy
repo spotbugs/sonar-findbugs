78d34503051d9b46ce441f2087ec2e4515048a49
Roberto Montero <roberto.montero@pivotree.com>
2020-11-10 11:53:38 +0100

    Updated spotbugs 4.1.4

--- generate_profiles/BuildXmlFiles.groovy
+++ generate_profiles/BuildXmlFiles.groovy
@@ -3,13 +3,13 @@ import FsbClassifier;
 import static FsbClassifier.*;
 @Grapes([
 
-    @Grab(group='com.github.spotbugs', module='spotbugs', version='4.1.2'),
+    @Grab(group='com.github.spotbugs', module='spotbugs', version='4.1.4'),
     @Grab(group='com.mebigfatguy.sb-contrib', module='sb-contrib', version='7.4.7'),
     @Grab(group='com.h3xstream.findsecbugs' , module='findsecbugs-plugin', version='1.10.1')]
 )
 
 
-FB = new Plugin(groupId: 'com.github.spotbugs', artifactId: 'spotbugs', version: '4.1.2')
+FB = new Plugin(groupId: 'com.github.spotbugs', artifactId: 'spotbugs', version: '4.1.4')
 CONTRIB = new Plugin(groupId: 'com.mebigfatguy.sb-contrib', artifactId: 'sb-contrib', version: '7.4.7')
 FSB = new Plugin(groupId: 'com.h3xstream.findsecbugs', artifactId: 'findsecbugs-plugin', version: '1.10.1')
 
