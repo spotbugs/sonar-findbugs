b1a0472d47eb3877e84ef16100ddb54b1172dacb
gtoison <guillaume.toison@tobam.fr>
2021-08-11 11:28:07 +0200

    Issue #368 Replaced the Findbugs copyright notice

--- src/test/java/org/sonar/plugins/findbugs/configuration/SimpleConfiguration.java
+++ src/test/java/org/sonar/plugins/findbugs/configuration/SimpleConfiguration.java
@@ -1,7 +1,5 @@
 /*
- * SonarQube Findbugs Plugin
- * Copyright (C) 2012 SonarSource
- * sonarqube@googlegroups.com
+ * SonarQube SpotBugs Plugin
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
