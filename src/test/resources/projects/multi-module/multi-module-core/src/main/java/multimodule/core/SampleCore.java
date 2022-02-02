98bffec23a53b76c7b748e8523c1d6e656faea38
gtoison <guillaume.toison@tobam.fr>
2022-02-02 21:57:54 +0100

    test: issue #275 reproduced
    Integration test illustrating that in a multi module gradle projects,
    classes from dependencies are analyzed when only classes of the module
    should be

---
+++ src/test/resources/projects/multi-module/multi-module-core/src/main/java/multimodule/core/SampleCore.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package multimodule.core;
+
+public class SampleCore {
+	private String field;
+
+	public int npe() {
+    System.out.println("test".toString());
+		return field.hashCode();
+	}
+}
---
+++ src/test/resources/projects/multi-module/multi-module-fx/src/main/java/multimodule/fx/SampleFx.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package multimodule.fx;
+
+import multimodule.core.SampleCore;
+
+public class SampleFx {
+	private SampleCore field;
+
+	public int npe() {
+    System.out.println("test".toString());
+		return field.npe();
+	}
+}
