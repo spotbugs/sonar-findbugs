a469d26289d8b34bf5004ecdcfb35e1cbd8fa16b
Guillaume Toison <86775455+gtoison@users.noreply.github.com>
2022-02-12 17:03:56 +0100

    fix: revert to SpotBugs 4.5.2 (#479)
    SpotBugs 4.5.3 surfaced a bug in FindSecBugs, revert to 4.5.2 until a
    new version with the fix is released, see:
    https://github.com/find-sec-bugs/find-sec-bugs/issues/668

--- generate_profiles/BuildXmlFiles.groovy
+++ generate_profiles/BuildXmlFiles.groovy
@@ -7,13 +7,13 @@ import groovy.json.JsonSlurper;
 
 @Grapes([
 
-    @Grab(group='com.github.spotbugs', module='spotbugs', version='4.5.3'),
+    @Grab(group='com.github.spotbugs', module='spotbugs', version='4.5.2'),
     @Grab(group='com.mebigfatguy.sb-contrib', module='sb-contrib', version='7.4.7'),
     @Grab(group='com.h3xstream.findsecbugs' , module='findsecbugs-plugin', version='1.11.0')]
 )
 
 
-FB = new Plugin(groupId: 'com.github.spotbugs', artifactId: 'spotbugs', version: '4.5.3')
+FB = new Plugin(groupId: 'com.github.spotbugs', artifactId: 'spotbugs', version: '4.5.2')
 CONTRIB = new Plugin(groupId: 'com.mebigfatguy.sb-contrib', artifactId: 'sb-contrib', version: '7.4.7')
 FSB = new Plugin(groupId: 'com.h3xstream.findsecbugs', artifactId: 'findsecbugs-plugin', version: '1.11.0')
 
