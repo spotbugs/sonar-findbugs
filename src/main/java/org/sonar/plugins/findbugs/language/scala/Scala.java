a5ca8cc9911a97ee951dee104e806202462387c6
gtoison <guillaume.toison@tobam.fr>
2022-04-16 11:01:26 +0100

    Adding private constructor per SonarQube recommendation

--- src/main/java/org/sonar/plugins/findbugs/language/scala/Scala.java
+++ src/main/java/org/sonar/plugins/findbugs/language/scala/Scala.java
@@ -3,4 +3,8 @@ package org.sonar.plugins.findbugs.language.scala;
 public class Scala {
 
     public static final String KEY = "scala";
+    
+    private Scala() {
+      // This class is not meant to be instantiated
+    }
 }
