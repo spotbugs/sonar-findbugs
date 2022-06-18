a5745c4c968246e8229bb7bd394ec1f237cb10ab
gtoison <guillaume.toison@tobam.fr>
2022-06-18 22:58:50 +0100

    fix: removed unused import

--- src/main/java/org/sonar/plugins/findbugs/resource/ByteCodeResourceLocator.java
+++ src/main/java/org/sonar/plugins/findbugs/resource/ByteCodeResourceLocator.java
@@ -23,7 +23,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
