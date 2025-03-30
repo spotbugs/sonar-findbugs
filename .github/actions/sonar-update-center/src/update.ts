52df75d51f79280bbeab7f07434c85bfc189f967
Jeremy Landis <jeremylandis@hotmail.com>
2025-03-29 23:22:02 -0400

    testing

--- .github/actions/sonar-update-center/src/update.ts
+++ .github/actions/sonar-update-center/src/update.ts
@@ -45,7 +45,7 @@ export async function update(
       ',LATEST]',
       `,${latestMinorVersion}]`
     )
-    core.info("Version seen is ${latestMinorVersion}")
+    core.info(`Version seen is ${latestMinorVersion}`)
     core.info(
       `Updating ${prevPublicVersions}.sqVersions from ${prevSqVersions} to ${updatedPrevSqVersions}...`
     )
