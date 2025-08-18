746228f336fec0b4d647b756cde0f83fa503a1a7
gtoison <guillaume.toison@tobam.fr>
2025-08-18 20:50:37 +0200

    deps: upgrade sb contrib to 7.6.13

--- generate_profiles/BuildXmlFiles.groovy
+++ generate_profiles/BuildXmlFiles.groovy
@@ -9,13 +9,13 @@ import groovy.json.JsonSlurper;
 @Grapes([
 
     @Grab(group='com.github.spotbugs', module='spotbugs', version='4.9.4'),
-    @Grab(group='com.mebigfatguy.sb-contrib', module='sb-contrib', version='7.6.12'),
+    @Grab(group='com.mebigfatguy.sb-contrib', module='sb-contrib', version='7.6.13'),
     @Grab(group='com.h3xstream.findsecbugs' , module='findsecbugs-plugin', version='1.14.0')]
 )
 
 
 FB = new Plugin(groupId: 'com.github.spotbugs', artifactId: 'spotbugs', version: '4.9.4')
-CONTRIB = new Plugin(groupId: 'com.mebigfatguy.sb-contrib', artifactId: 'sb-contrib', version: '7.6.12')
+CONTRIB = new Plugin(groupId: 'com.mebigfatguy.sb-contrib', artifactId: 'sb-contrib', version: '7.6.13')
 FSB = new Plugin(groupId: 'com.h3xstream.findsecbugs', artifactId: 'findsecbugs-plugin', version: '1.14.0')
 
 def destDir() {
