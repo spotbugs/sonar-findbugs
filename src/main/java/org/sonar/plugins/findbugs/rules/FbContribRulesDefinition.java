a4a9c4f30662291434065c38f9607d5205e278ab
Judit Knoll <knoll.judit94@gmail.com>
2025-12-09 09:36:50 +0100

    Upgrade rules to sb-contrib to 7.7.2

--- src/main/java/org/sonar/plugins/findbugs/rules/FbContribRulesDefinition.java
+++ src/main/java/org/sonar/plugins/findbugs/rules/FbContribRulesDefinition.java
@@ -27,7 +27,7 @@ public class FbContribRulesDefinition implements RulesDefinition {
 
   public static final String REPOSITORY_KEY = "fb-contrib";
   public static final String REPOSITORY_NAME = "FindBugs Contrib";
-  public static final int RULE_COUNT = 331;
+  public static final int RULE_COUNT = 332;
   public static final int DEACTIVED_RULE_COUNT = 0;
 
   @Override
